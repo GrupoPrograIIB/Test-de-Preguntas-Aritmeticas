09c60558abad1b645d076af98b86f4e42eeddec8
Camila2206 <33434428+Camila2206@users.noreply.github.com>
2017-12-04 18:38:43 -0500

    Update PreguntaAdicion.java

--- TestAritmetico/src/testaritmetico/PreguntaAdicion.java
+++ TestAritmetico/src/testaritmetico/PreguntaAdicion.java
@@ -5,7 +5,6 @@
  */
 package testaritmetico;
 
-
 /**
  *
  * @author Camila Cueva
@@ -13,20 +12,38 @@ package testaritmetico;
 public class PreguntaAdicion implements PreguntaEntera{
     private int a,b;
     
+    
+    /**
+     * @param PreguntaAdicion() sera nuestro constructor en el cual this.a tendra 
+     * un valor entero aleatorioentre ente (Math.random()*50+1).
+     * De la misma forma this.b tendra el valor entero aleatorio entre (Math.random()*50)
+     */
     public  PreguntaAdicion(){
         this.a= (int) (Math.random()*50+1);
         this.b= (int)(Math.random()*50); 
     }
     
+    
+    /**
+     * @param getPregunta() será el método sobre escrito para la clase PreguntaAdicion en el cual se hace la pregunta al usuario
+     * sobre cuál será el resultado de la adición de los valores asignados para a y b. 
+     */
+        
     @Override
     public String getPregunta(){
         return "Cuanto es:"+a+" + "+b+"?";
     }
+    
+    
+    /**
+     * @param getRespuestaCorrecta() sera el método sobre escrito para la clase PreguntaAdicion en la que se realizará la suma
+     * de los valores asignados para a y b; 
+     */
+    
     @Override
     public int getRespuestaCorrecta(){
         return a+b;
     }
-
 
     
 }
