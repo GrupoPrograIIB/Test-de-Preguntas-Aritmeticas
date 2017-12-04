e752de77f29aed8332502b38c63e5a92217562b2
Camila2206 <33434428+Camila2206@users.noreply.github.com>
2017-12-04 18:39:45 -0500

    Update PreguntaMultiplicacion.java

--- TestAritmetico/src/testaritmetico/PreguntaMultiplicacion.java
+++ TestAritmetico/src/testaritmetico/PreguntaMultiplicacion.java
@@ -13,15 +13,34 @@ package testaritmetico;
 public class PreguntaMultiplicacion implements PreguntaEntera{
     private int a,b;
     
+    /**
+     * @param PreguntaMultiplicacion() sera nuestro constructor en el cual this.a tendra 
+     * un valor entero aleatorioentre ente (Math.random()*50+1).
+     * De la misma forma this.b tendra el valor entero aleatorio entre (Math.random()*50)
+     */
+    
     public PreguntaMultiplicacion(){
         this.a = (int) (Math.random() * 50 + 1);
         this.b = (int) (Math.random() * 50);
     }
     
+    
+    /**
+     * @param getPregunta() será el método sobre escrito para la clase PreguntaMultiplicacion en el cual se hace la pregunta al usuario
+     * sobre cuál será el resultado de la multiplicación de los valores asignados para a y b. 
+     */
+    
     @Override
      public String getPregunta(){
         return "Cuanto es:"+a+" * "+b+"?";
     }
+    
+         
+    /**
+     * @param getRespuestaCorrecta() sera el método sobre escrito para la clase PreguntaMultiplicacion en la que se realizará la 
+     * multiplicación de los valores asignados para a y b;
+     */
+    
     @Override
     public int getRespuestaCorrecta(){
         return a*b;
